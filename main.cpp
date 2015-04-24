79e24d7a13730c856f1b8847af41c7c68b2a11a4
audunsh <audunsh@student.matnat.uio.no>
2015-04-24 18:35:53 +0200

    Changing all indexing uints -> ints due to system size exceeding index counters

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ using namespace arma;
 int main()
 {
     electrongas fgas;
-    fgas.generate_state_list(25.0,1.0, 14);
+    fgas.generate_state_list(35.0,1.0, 14);
 
     //cout << "Energy per particle:" << fgas.eref(14)/14.0 << " (a.u)"  << endl;
     cout << "# Energy per particle:" << 2*fgas.eref(14)/14.0 << " (rydberg)"  << endl;
